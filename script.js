97e7792a5e8715e507dbaa417760cc76bb58d673
Riley Harper <riley.harper@unc.edu>
2024-02-15 06:15:16 +0000

    Javascript Testing w Hardcoded url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('submitBtn').addEventListener('click', function(e) {
     e.preventDefault();
     window.location.href = 'https://github.com/rmharp/Zoom/blob/main/media/zoom/zoom_Part17.pdf'; // Hardcoded URL for testing
 });
